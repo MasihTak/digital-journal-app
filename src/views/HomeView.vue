1c3802db320f105408b7c5545476b20b4e4e632a
MasihTak <masihtak@outlook.com>
2023-12-30 17:49:31 +0100

    style: make the get started button larger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,6 @@ import {RouterLink} from 'vue-router'
 <template>
   <div class="d-flex flex-column align-items-center justify-content-center h-100">
     <h1 class="text-center">Digital Journal <br />App</h1>
-    <RouterLink to="/register" class="btn btn-primary mt-5">Get started</RouterLink>
+    <RouterLink to="/register" class="btn btn-primary btn-lg mt-5">Get started</RouterLink>
   </div>
 </template>

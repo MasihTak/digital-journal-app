4477cead7c68ad081679f05f7e5a10d936a0f41c
MasihTak <masihtak@outlook.com>
2023-12-31 08:36:33 +0100

    feat: add auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/sign-up',
+            name: 'signUp',
+            component: () => import('../views/auth/SignUp.vue')
+        },
+        {
+            path: '/sign-in',
+            name: 'signIn',
+            component: () => import('../views/auth/SignIn.vue')
+        },
+        {
+            path: '/notes',
+            name: 'notes',
+            component: () => import('../views/TheNotes.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue')
+        }
+    ]
 })
 
 export default router

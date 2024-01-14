ab3f4ca86eb11f039f02c0c2fc51345cf0de729a
MasihTak <masihtak@outlook.com>
2024-01-14 16:15:48 +0100

    fix: save journal

--- src/views/NewJournal.vue
+++ src/views/NewJournal.vue
@@ -1,20 +1,17 @@
 <script setup>
 import {ref} from 'vue';
-import {useAuthStore} from "@/stores/auth";
 import {useJournalStore} from "@/stores/journal";
 import {VMarkdownEditor} from 'vue3-markdown';
 import 'vue3-markdown/dist/style.css';
 import BaseInput from "@/components/Form/BaseInput.vue";
 
-const {getUser} = useAuthStore();
 const {createJournal} = useJournalStore();
 
 const content = ref('')
 const title = ref('')
-const user_id = getUser().id
 
 function saveJournal() {
-  createJournal(title.value, content.value, user_id)
+  createJournal(title.value, content.value)
 }
 </script>
 

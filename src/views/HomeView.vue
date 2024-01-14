1085c9dbe1ed52a98007a06e565c39624a54f82f
MasihTak <masihtak@outlook.com>
2024-01-14 11:29:14 +0100

    feat: add logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,15 @@ import {RouterLink} from 'vue-router'
 
 <template>
   <div class="d-flex flex-column align-items-center justify-content-center h-100">
+    <img src="@/assets/img/logo.svg" alt="logo" class="img-fluid mb-3 logo" />
     <h1 class="text-center">Digital Journal <br />App</h1>
     <RouterLink to="/sign-up" class="btn btn-primary btn-lg mt-5">Get started</RouterLink>
   </div>
 </template>
+
+<style scoped lang="scss">
+img {
+  width  : 175px;
+  height : 175px;
+  }
+</style>

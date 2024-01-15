e93882710afd3f2e01391c5d7cbed99a1dafd6da
MasihTak <masihtak@outlook.com>
2024-01-15 17:42:52 +0100

    feat: add journal card component

---
+++ src/components/UI/JournalCard.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {formatTimeAgo} from '@vueuse/core';
+import {VMarkdownView} from 'vue3-markdown'
+import 'vue3-markdown/dist/style.css'
+
+defineEmits(['deleteJournal', 'editJournal'])
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <router-link :to="url">
+    <div class="card shadow-sm">
+      <div class="card-body">
+        <h5 class="card-title">{{ title }}</h5>
+        <VMarkdownView class="mb-5"
+                       :content="content"
+        ></VMarkdownView>
+        <button type="button"
+                class="btn btn-primary me-2"
+                @click="$emit('editJournal')"
+        >Edit
+        </button>
+        <button type="button"
+                class="btn btn-danger"
+                @click="$emit('deleteJournal')"
+        >Delete
+        </button>
+      </div>
+      <div class="card-footer text-body-secondary">
+        {{ formatTimeAgo(createdAt) }}
+      </div>
+    </div>
+  </router-link>
+</template>

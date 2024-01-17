8a4ad1fa0d078e9c361ded431ec23cc11d360568
MasihTak <masihtak@outlook.com>
2024-01-17 12:08:03 +0100

    style: minor ui improvements

--- src/components/UI/JournalCard.vue
+++ src/components/UI/JournalCard.vue
@@ -3,8 +3,10 @@ import {formatTimeAgo} from '@vueuse/core';
 import {VMarkdownView} from 'vue3-markdown'
 import 'vue3-markdown/dist/style.css'
 
+/*----------- Emits -----------*/
 defineEmits(['deleteJournal', 'editJournal'])
 
+/*----------- Props -----------*/
 defineProps({
   title: {
     type: String,
@@ -26,11 +28,11 @@ defineProps({
 </script>
 
 <template>
-  <router-link :to="url">
-    <div class="card shadow-sm">
+  <router-link class="text-decoration-none" :to="url">
+    <div class="card shadow-sm border-light-subtle h-100">
       <div class="card-body">
         <h5 class="card-title">{{ title }}</h5>
-        <VMarkdownView class="mb-5"
+        <VMarkdownView class="mb-5 d-block text-truncate"
                        :content="content"
         ></VMarkdownView>
         <button type="button"
